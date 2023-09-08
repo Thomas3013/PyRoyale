d1e04338d88d0a758de7d4ce51f522d0f82d95b4
wwingss <penislicer8@gmail.com>
2023-09-08 15:17:47 -0700

    + window display icon + game sounds (menu theme) + assets folder

--- main.py
+++ main.py
@@ -9,7 +9,12 @@ screen_width = 720          #placeholder resolutions
 screen_height = 720
 screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption('Clash Royale')
+icon = pygame.image.load('assets/icon.png')
+pygame.display.set_icon(icon)
 
+#game sounds
+pygame.mixer.music.load('assets/menu_theme.mp3')
+pygame.mixer.music.play(-1)
 
 
 # GAME LOGIC
