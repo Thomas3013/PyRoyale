3db3bb858061d660dfbfa4f11be8c1f68fb15ff1
wwingss <penislicer8@gmail.com>
2023-09-08 01:27:02 -0700

    basic main file setup

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+
+#  WINDOW DISPLAY
+screen_width = 1280          #placeholder resolutions
+screen_height = 720
+screen = pygame.display.set_mode((screen_width,screen_height))
+pygame.display.set_caption('Clash Royale')
+
+
+
+# GAME LOGIC
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    pygame.display.update
+
+pygame.quit()
