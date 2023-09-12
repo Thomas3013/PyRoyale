b45aac34c415ca994747ce108f6432f5fb5c1e61
seatbuckle <107852901+seatbuckle@users.noreply.github.com>
2023-09-11 19:40:16 -0700

    Update main.py
    fixed alot of shit, hate carrot,

--- main.py
+++ main.py
@@ -1,96 +1,128 @@
 import pygame
-import time
-import threading
 from pygame.locals import *
 
 pygame.init()
 
-
-#  WINDOW DISPLAY
-screen_width = 720          #placeholder resolutions
-screen_height = 720
-screen = pygame.display.set_mode((screen_width,screen_height))
+# WINDOW DISPLAY
+screen_width = 540
+screen_height = 960
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Clash Royale')
 icon = pygame.image.load('assets/icon.png')
 pygame.display.set_icon(icon)
 
+# Calculate the position to center the arena horizontally and align it to the top vertically
+arena_width = 430  # Width of the arena
+arena_height = 720  # Height of the arena
+arena_x = (screen_width - arena_width) // 2
+arena_y = 0
 
 # GAME LOGIC
-elixir = 0
-elixir_speed = 2.8
+current_elixir = 0  # Initialize current_elixir to 0
+max_elixir = 10
+elixir_generation_rate = 1 / 2.8  # Elixir every 2.8 seconds
 run = True
-game_time = 300
+background = pygame.image.load('assets/bg.png')
 
+elixir_bar_images = []
+for i in range(11):
+    image = pygame.image.load(f'assets/elixir{i}.png')
+    image = pygame.transform.scale(image, (540, 30))
+    elixir_bar_images.append(image)
 
-#image loader
-elixir_state = []
-for elixir_level in range(0,11):
-    eimg = pygame.image.load(f'assets/elixir{elixir_level}.png')
-    elixir_state.append(eimg)
-bg = pygame.image.load('assets/bg.png')    
+def display_elixir_bar(screen, current_elixir):
+    # Ensure the elixir is within the valid range (0-10)
+    current_elixir = max(0, min(10, current_elixir))
 
-#game sounds
+    # Calculate the index of the elixir bar image
+    elixir_bar_index = int(current_elixir)
+
+    # Get the corresponding elixir bar image
+    elixir_bar_image = elixir_bar_images[elixir_bar_index]
+
+    # Display the elixir bar image at the bottom of the screen
+    screen.blit(elixir_bar_image, (0, screen_height - 30))
+
+# Get the current time in milliseconds
+current_time = pygame.time.get_ticks()
+start_time = pygame.time.get_ticks()  # Record the starting time in milliseconds
+time_limit = 300000  # 5 minutes in milliseconds (5 * 60 * 1000)
+
+# game sounds
 pygame.mixer.music.load('assets/menu_theme.mp3')
 pygame.mixer.music.play(-1)
 
-#game font
+# game font
 font = pygame.font.Font('assets/Helvetica_Rounded_Bold.otf', 36)
 
-def game_timer():
-    global game_time
-    while True:
-        # update time and generate new image for current time
-        if game_time > 0:
-            game_time -= 1
-        convert = time.strftime("%M:%S", time.gmtime(game_time))
-        time_img = font.render(convert, True, (255,255,255))
-        # create surface to draw time onto
-        screen_surface = pygame.Surface((200, 100))
-        screen_surface.fill((0,0,0))
-        # draw updated time onto screen
-        screen_surface.blit(time_img,(0,0))
-        screen.blit(screen_surface, (600, 50))
-        pygame.display.update()
+# Store the current time as the last time
+last_time = current_time
 
-        time.sleep(1)
-        print(convert)
-
-def elixir_counter():
-    global elixir, elixir_speed, screen
-    while True:
-        # updating on-screen elixir to match elixir value  
-        screen.blit(elixir_state[elixir], (72, 627))
-        pygame.display.update()
-        #double elixir grants +1 elixir to player
-        if game_time == 180:
-            if(elixir<10):
-                elixir += 1
-                print("elixir gained", elixir)  
-        # double elixir
-        if game_time <= 180:
-            elixir_speed = 1.4
-        # triple elixir
-        if game_time == 60:
-            elixir_speed = 0.9
-        # elixir cap
-        if elixir < 10:
-            elixir += 1
-
-        time.sleep(elixir_speed)
-        print("elixir gained", elixir)
-
-elixir_thread = threading.Thread(target=elixir_counter)
-elixir_thread.daemon = True
-elixir_thread.start()
-
-gametimer_thread = threading.Thread(target=game_timer)
-gametimer_thread.daemon = True
-gametimer_thread.start()
+# Define the size of the timer background
+timer_bg_width = 100  # Adjust this as needed
+timer_bg_height = 50  # Adjust this as needed
 
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    pygame.display.update
+
+    # Get the current time in milliseconds
+    current_time = pygame.time.get_ticks()
+
+    # Calculate the elapsed time in milliseconds since the last frame
+    elapsed_time = current_time - last_time
+
+    # Calculate the time required for 1 elixir to generate (2.8 seconds in milliseconds)
+    elixir_generation_time = 2.8 * 1000  # 2.8 seconds to milliseconds
+
+    # Update elixir amount based on the elapsed time and generation rate
+    elixir_to_generate = elapsed_time / elixir_generation_time
+    current_elixir += elixir_to_generate
+
+    # Ensure elixir doesn't exceed the cap
+    current_elixir = min(current_elixir, max_elixir)
+
+    # Update the last_time to the current_time for the next frame
+    last_time = current_time
+
+    # Calculate the remaining time in milliseconds
+    remaining_time = time_limit - (current_time - start_time)
+
+    # Ensure the timer doesn't go below 0
+    remaining_time = max(remaining_time, 0)
+
+    # Convert remaining time to seconds
+    timer = remaining_time // 1000  # Convert milliseconds to seconds
+
+    # Calculate minutes and seconds
+    minutes = timer // 60
+    seconds = timer % 60
+
+    # Convert to "MM:SS" format
+    timer_text = f"{minutes:02}:{seconds:02}"
+
+    # Create a semi-transparent grey background for the timer text
+    timer_bg_surface = pygame.Surface((timer_bg_width, timer_bg_height), pygame.SRCALPHA)
+    pygame.draw.rect(timer_bg_surface, (128, 128, 128, 128), timer_bg_surface.get_rect())
+
+    screen.fill((0, 0, 0))  # Fill the screen with a black background
+
+    # Draw the arena in the center of the screen
+    screen.blit(background, (arena_x, arena_y))
+
+    # Call the function to display the elixir bar
+    display_elixir_bar(screen, current_elixir)
+
+    # Blit the timer background onto the screen in the top right corner
+    timer_bg_rect = timer_bg_surface.get_rect(topright=(screen_width - 10, 10))
+    screen.blit(timer_bg_surface, timer_bg_rect)
+
+    # Display the timer text on top of the timer background
+    timer_surface = font.render(timer_text, True, (255, 255, 255))
+    timer_rect = timer_surface.get_rect(topright=(screen_width - 10, 10))
+    screen.blit(timer_surface, timer_rect)
+
+    pygame.display.update()
 
 pygame.quit()
